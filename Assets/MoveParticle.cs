f0701d17470f2fcb4ca51fe91bcc6622807d325d
blackBoyCode <e96e6e6@gmail.com>
2019-11-14 20:59:27 -0500

    redo stuff

---
+++ Assets/MoveParticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveParticle : MonoBehaviour
+{
+
+    public Transform destination;
+
+    public float speed = 1f;
+
+    private Vector3 originalPosition;
+    private float counter = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.originalPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.counter += Time.deltaTime * this.speed;
+        this.counter = Mathf.Clamp01(this.counter);
+        this.transform.position = Vector3.Lerp(this.originalPosition, this.destination.position, 1-this.counter);
+    }
+}
---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+
+    public GameObject targetToRotateAround;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindWithTag("Character");
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMovement();
+        
+    }
+
+
+    void CameraMovement()
+    {
+        
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+        float mouseVertical = Input.GetAxis("Mouse Y");
+        
+        transform.RotateAround(targetToRotateAround.transform.position, Vector3.up, mouseHorizontal * Time.deltaTime);
+        
+        player.transform.Rotate(Vector3.up,mouseHorizontal * 100 * Time.deltaTime);
+        
+    }
+}
