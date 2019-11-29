3c0abdd7ee41a9db09f8084479618979cf8c2a26
gearsofalegend <47233008+gearsofalegend@users.noreply.github.com>
2019-11-28 23:11:47 -0500

    Tunnel Stuff (lights, obstacles) To do: more tunnel Shimmy, Rotation, then MAYBE...

---
+++ Assets/TryGym/Scripts_Try/Shimmy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shimmy : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    public float speedZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Mathf.Cos(Time.time * 1) * speedX, Mathf.Cos(Time.time * 1) * speedY, Mathf.Cos(Time.time * 1) * speedZ);
+    }
+}
